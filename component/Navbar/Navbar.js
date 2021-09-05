c8f0a515178c856b3b2e1b68666ac43c370c10f9
asen23 <andersenlim23@gmail.com>
2021-09-05 11:16:31 +0700

    feat: added home link to navbar

--- component/Navbar/Navbar.js
+++ component/Navbar/Navbar.js
@@ -1,9 +1,15 @@
 import Link from "next/link"
-import styles from './Navbar.module.css'
+import styles from "./Navbar.module.css"
 
 export default function Navbar() {
-    return <nav className={`${styles.container}`}>
-        <h1>Random API</h1>
-        <div></div>
-    </nav>
+    return (
+        <nav className={`${styles.container}`}>
+            <Link href="/">
+                <a>
+                    <h1>Random API</h1>
+                </a>
+            </Link>
+            <div></div>
+        </nav>
+    )
 }
