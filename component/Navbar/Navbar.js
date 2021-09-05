6c5872205feb345e0f05fd128e56b44b5c278d6f
asen23 <andersenlim23@gmail.com>
2021-09-05 10:29:53 +0700

    feat: added navbar and connect to home

---
+++ component/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import Link from "next/link"
+import styles from './Navbar.module.css'
+
+export default function Navbar() {
+    return <nav className={`${styles.container}`}>
+        <h1>Random API</h1>
+        <div></div>
+    </nav>
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head"
 import Image from "next/image"
+import Navbar from "../component/Navbar/Navbar"
 import styles from "../styles/Home.module.css"
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
                 <meta name="description" content="A collection of random api" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+            <Navbar />
             <div className={`${styles.container}`}>
                 <h1>Welcome to random API</h1>
                 <h2>This website provide random API interfaces</h2>
