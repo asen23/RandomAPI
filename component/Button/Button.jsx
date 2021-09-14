440fa236ced9b6d862557256cf23d93717698f54
asen23 <andersenlim23@gmail.com>
2021-09-14 11:29:39 +0700

    feat: changed button style to tailwind

--- component/Button/Button.jsx
+++ component/Button/Button.jsx
@@ -1,8 +1,6 @@
-import styles from "./Button.module.css"
-
 export default function Button(props) {
     return (
-        <button onClick={props.onClick} className={styles.button}>
+        <button onClick={props.onClick} className="border border-gray-400 px-2 py-1 rounded-lg hover:bg-white">
             {props.children}
         </button>
     )
