b4c12d0a339ee45c9c90ce8124a7a388af13199b
asen23 <andersenlim23@gmail.com>
2021-09-14 11:03:28 +0700

    feat: changed navbar style to tailwind

--- component/Navbar/Navbar.jsx
+++ component/Navbar/Navbar.jsx
@@ -1,33 +1,32 @@
 import Link from "next/link"
-import styles from "./Navbar.module.css"
 
 export default function Navbar() {
     return (
-        <nav className={`${styles.container}`}>
+        <nav className="flex">
             <Link href="/">
-                <a className={`${styles.home}`}>
+                <a className="text-2xl p-4 font-bold">
                     <h1>Random API</h1>
                 </a>
             </Link>
-            <div className={`${styles.linkGroup}`}>
+            <div className="flex flex-1 flex-row-reverse px-2">
                 <Link href="/joke">
                     <a>
-                        <div className={`${styles.link}`}>Joke</div>
+                        <div className="">Joke</div>
                     </a>
                 </Link>
                 <Link href="/quote">
                     <a>
-                        <div className={`${styles.link}`}>Quote</div>
+                        <div className="">Quote</div>
                     </a>
                 </Link>
                 <Link href="/waifu">
                     <a>
-                        <div className={`${styles.link}`}>Waifu</div>
+                        <div className="">Waifu</div>
                     </a>
                 </Link>
                 <Link href="/numberfact">
                     <a>
-                        <div className={`${styles.link}`}>Number Fact</div>
+                        <div className="">Number Fact</div>
                     </a>
                 </Link>
             </div>
