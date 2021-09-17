eb8367f3f818b2c7fdc1ebfd862375e61ffd22be
asen23 <andersenlim23@gmail.com>
2021-09-17 10:30:05 +0700

    ref: change hardcoded color to variable

--- component/Container.jsx
+++ component/Container.jsx
@@ -1,6 +1,6 @@
 export default function Container(props) {
     return (
-        <div className="flex justify-center items-center flex-col min-h-screen gap-8 bg-[antiquewhite]">
+        <div className="flex justify-center items-center flex-col min-h-screen gap-8 bg-primary">
             {props.children}
         </div>
     )
