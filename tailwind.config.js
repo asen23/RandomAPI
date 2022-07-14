069feb71305b38d8754fefe4c268e703b778212d
asen23 <andersenlim23@gmail.com>
2022-07-14 13:17:46 +0700

    feat: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     mode: "jit",
-    purge: ["./pages/**/*.jsx", "./component/**/*.jsx"],
+    content: ["./pages/**/*.jsx", "./component/**/*.jsx"],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
